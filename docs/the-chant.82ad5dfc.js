d21a12c6943ce3dc175d1d8ab29c98ce67a0c6a4
Felipe Lara <70984535+Aosiee@users.noreply.github.com>
2022-08-11 14:59:48 -0700

    Updated Docs

---
+++ docs/the-chant.82ad5dfc.js
@@ -0,0 +1,7 @@
+/*!
+ * Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+ */
+window.addEventListener("DOMContentLoaded",(e=>{var n=function(){const e=document.body.querySelector("#mainNav");e&&(0===window.scrollY?e.classList.remove("navbar-shrink"):e.classList.add("navbar-shrink"))};n(),document.addEventListener("scroll",n);const o=document.body.querySelector(".navbar-toggler");[].slice.call(document.querySelectorAll("#navbarResponsive .nav-link")).map((function(e){e.addEventListener("click",(()=>{"none"!==window.getComputedStyle(o).display&&o.click()}))}))}));
+//# sourceMappingURL=the-chant.82ad5dfc.js.map
