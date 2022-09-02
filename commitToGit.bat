ad15284e480225f9038a246d68494570014c5d82
Felipe Lara <70984535+Aosiee@users.noreply.github.com>
2022-09-02 00:33:57 -0700

    - Automated Commit - 2022-09-02-00-33-53-99

--- commitToGit.bat
+++ commitToGit.bat
@@ -3,19 +3,30 @@ set dateTime=%2
 
 set /p customGitMessage="Add Custom Commit Message: "
 
+::--------------------------------------------------------
 ::Commit Message Add Custom // Auto Commit from Bat File
 set gitMessage="%customGitMessage% - Automated Commit - %dateTime%"
-
+::--------------------------------------------------------
 ::Safety Pull
 git pull >> %outputLog%
-
+::--------------------------------------------------------
 ::Add Changes to Directory
 git add %~dp0\ >> %outputLog%
+::--------------------------------------------------------
+::If Blank, Skip to Generic Commit, Else Continue
+if %gitMessage%=="" goto genericCommit
+::--------------------------------------------------------
 
-if %gitMessage%=="" set %gitMessage%="Generic Push"
-
-::Commit
+::Custom Message Commit
 git commit -am %gitMessage% >> %outputLog%
+goto push
 
-::Push
+::Generic Message if != Custom Message
+:genericCommit
+
+set customGitMessage="Generic Commit"
+git commit -am %gitMessage% >> %outputLog%
+goto push
+
+:push
 git push >> %outputLog%
