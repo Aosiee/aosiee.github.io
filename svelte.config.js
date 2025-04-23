6f4c3d8c9e05b719967b133baf23985c4a468ad8
Felipe Lara <70984535+Aosiee@users.noreply.github.com>
2025-04-23 14:00:41 -0700

    Potential Fix

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 			fallback: null
 		}),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/aosiee.github.io' : '',
+            base: '',
         }
 	}
 };
